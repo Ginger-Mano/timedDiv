7f8a2f9e0ac15c76906b62e8460add9a79eb04e5
Manon Sainton <manonsainton.dev@gmail.com>
2021-05-12 17:18:05 -0400

    add start btn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 let appBase = document.querySelector(".base")
 
-let count = 0
-let click = true
+let count = 0,
+    count2 = 0,
+    count3 = 0,
+    count4 = 0,
+    initiate,
+    one,
+    two,
+    three,
+    four;
 
 let showDiv = (() => {
     let showDivMain = document.createElement("div")
@@ -11,65 +18,43 @@ let showDiv = (() => {
     firstCount.className = "header"
     firstCount.innerHTML = count
 
-  
-    firstCount.addEventListener("click", (evt) => {
-        
-        setInterval(function() {firstCount.innerHTML = count++}, 1000);
-    })
-
-
     let secondCount = document.createElement('button')
     secondCount.className = "header"
     secondCount.innerHTML = count
 
-    secondCount.onclick = () => {
-        let count2 = 0
-        setInterval(function() {secondCount.innerHTML = count2++}, 250);
-    }
-
-    // let tcDiv = document.createElement("div")
-    // tcDiv.className = "btnDiv"
     let thirdCount = document.createElement('button')
     thirdCount.className = "header"
     thirdCount.innerHTML = count
 
-   thirdCount.addEventListener("click", (evt) => {
-        let count3 = 0 
-        setInterval(function () {thirdCount.innerHTML = count3 += 5}, 1000);
-    })
-
-    // let fthcDiv = document.createElement("div")
-    // fthcDiv.className = "btnDiv"
     let fourthCount = document.createElement('button')
     fourthCount.className = "header"
     fourthCount.innerHTML = count
 
-    fourthCount.addEventListener("click", (evt) => {
-        
-        let count4 = 0 
-        let timed = setInterval(set, 50);
+    let start = document.createElement('button')
+    start.innerHTML = "start"
 
-        function set() {fourthCount.innerHTML = count4 += 5}
-        
-        if (click = !true) {
-            timeStop()
-        }
-        
-        function timeStop() {
+    start.addEventListener("click", (evt) => {
+        one = setInterval(function() {firstCount.innerHTML = count++}, 1000);
+        two = setInterval(function() {secondCount.innerHTML = count2++}, 250);
+        three = setInterval(function () {thirdCount.innerHTML = count3 += 5}, 1000);
+        four = setInterval(function (){fourthCount.innerHTML = count4 += 5}, 500)
+    })
+
+    let reset = document.createElement('button')
+    reset.innerHTML = "reset"
+
+    reset.addEventListener("click", (evt) => {
        
-          clearInterval(timed)
-          
-    }
+    })
 
+    let pause = document.createElement("button")
+    pause.innerHTML = "pause"
+
+    pause.addEventListener("click", (evt) => {
+       
     })
 
 
-
-    // fcDiv.append(firstCount)
-    // scDiv.append(secondCount)
-    // tcDiv.append(thirdCount)
-    // fthcDiv.append(fourthCount)
-    // showDivMain.append(fcDiv, scDiv, tcDiv, fthcDiv)
-    showDivMain.append(firstCount, secondCount, thirdCount, fourthCount)
+    showDivMain.append(firstCount, secondCount, thirdCount, fourthCount, start, reset, pause)
     appBase.append(showDivMain)
 })()
